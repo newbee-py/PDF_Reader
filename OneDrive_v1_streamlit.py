59f8042690a36335f91e86091427a32b38a386eb
newbee-py <59944846+newbee-py@users.noreply.github.com>
2025-02-13 15:16:31 -0800

    OneDrive_v1_streamlit.py

--- OneDrive_v1_streamlit.py
+++ OneDrive_v1_streamlit.py
@@ -45,7 +45,7 @@ embedding_model = SentenceTransformer(EMBEDDING_MODEL_NAME, device=device)
 chroma_client = chromadb.PersistentClient(path="./chroma_db")
 chroma_collection = chroma_client.get_or_create_collection(name="my_collection")
 
-onedrive_folder = r"C:\Users\jatin.malhotra\OneDrive - HCL TECHNOLOGIES LIMITED\Documents\SON Documents\23A.1"
+onedrive_folder = os.path.join("C:", "Users", "jatin.malhotra", "OneDrive - HCL TECHNOLOGIES LIMITED", "Documents", "SON Documents", "23A.1")
 if os.path.exists(onedrive_folder):
     print("Folder exists:", os.listdir(onedrive_folder))  # Lists all files
 else:
